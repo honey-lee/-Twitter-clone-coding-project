84447d680d52ae013b397d9afdd8a62dc71b9e3a
hwajeong <honeylee999@icloud.com>
2021-08-29 23:52:13 +0900

    회원 이미지 정보 변경

---
+++ src/components/ProfileEditModal.vue
@@ -0,0 +1,102 @@
+<template>
+  <div @click="$emit('close-modal')" class="fixed z-10 inset-0 overflow-y-auto" aria-labelledby="modal-title" role="dialog" aria-modal="true">
+    <div class="flex justify-center min-h-screen sm:pt-6 sm:px-4 sm:pb-20 text-center sm:block sm:p-0">
+      <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" aria-hidden="true"></div>
+      <!-- contents -->
+      <div @click.stop class="inline-block bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg w-full">
+        <div class="border-b border-gray-100 p-2 flex items-center justify-between">
+          <div class="flex items-center">
+            <button @click="$emit('close-modal')" class="flex items-center justify-center fas fa-times text-primary text-lg p-2 h-10 w-10 hover:bg-blue-50 rounded-full"></button>
+            <span class="font-bold text-lg">프로필 수정</span>
+          </div>
+          <div class="text-right mr-2">
+            <button class="hover:bg-dark bg-primary text-white font-bold px-3 py-1 rounded-full">저장</button>
+          </div>
+        </div>
+        <!-- tweeting section -->
+        <div class="h-60">
+          <!-- background image -->
+          <div class="bg-gray-300 h-40 relative flex-none flex items-center justify-center">
+            <img ref="backgroundImage" src="/background.png" class="object-cover absolute h-full w-full" alt="">
+            <button @click="onChangeBackgroundImage" class="absolute h-10 w-10 hover:text-gray-200 rounded-full fas fa-camera text-white text-lg"></button>
+            <input @change="previewBackgroundImage" type="file" accept="image/*" id="backgroundImageInput" class="hidden">
+          
+            <!-- profile image -->
+            <img ref="profileImage" src="/profile.jpeg" alt="" class="border-4 border-white w-28 h-28 absolute -bottom-14 left-2 rounded-full" />
+            <button @click="onChangeProfileImage" class="absolute h-10 w-10 hover:text-gray-200 rounded-full fas fa-camera text-white text-lg -bottom-5 left-11"></button>
+            <input @change="previewprofileImage" type="file" accept="image/*" id="profileImageInput" class="hidden">
+          </div>
+        </div>
+        
+      </div>
+    </div>
+  </div>
+
+</template>
+
+<script>
+import { ref, computed } from 'vue'
+import addTweet from '../utils/addTweet'
+import store from '../store'
+export default {
+  setup(props, { emit }) {
+    const tweetBody = ref('')
+    const currentUser = computed(() => store.state.user)
+    
+    const profileImage = ref(null)
+    const backgroundImage = ref(null)
+
+    const onAddTweet = async() => {
+      try {
+        await addTweet(tweetBody.value, currentUser.value)
+        tweetBody.value = ''
+        emit('close-modal')
+        } catch (e) {
+        console.log('on add tweet error on homepage:', e)
+      }
+    }
+
+    const onChangeBackgroundImage = () => {
+      document.getElementById('backgroundImageInput').click()
+    }
+
+    const onChangeProfileImage = () => {
+      document.getElementById('profileImageInput').click()
+    }
+
+    const previewBackgroundImage = (event) => {
+      const file = event.target.files[0]
+      let reader = new FileReader()
+      reader.onload = function (event) {
+        backgroundImage.value.src = event.target.result
+      }
+      reader.readAsDataURL(file)
+    }
+
+    const previewprofileImage = (event) => {
+      const file = event.target.files[0]
+      let reader = new FileReader()
+      reader.onload = function (event) {
+        profileImage.value.src = event.target.result
+      }
+      reader.readAsDataURL(file)
+    }
+
+    return {
+      tweetBody,
+      onAddTweet,
+      currentUser,
+      onChangeBackgroundImage,
+      onChangeProfileImage,
+      previewBackgroundImage,
+      previewprofileImage,
+      backgroundImage,
+      profileImage,
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
--- src/pages/Profile.vue
+++ src/pages/Profile.vue
@@ -1,22 +1,22 @@
 <template>
-  <div class="flex-1 flex">
+  <div class="flex-1 flex" v-if="profileUser">
     <!-- profile section -->
     <div class="flex-1 flex flex-col border-r border-color">
       <!-- title -->
       <div class="px-3 py-1 flex border-b border-color">
-        <button class="mr-4">
+        <button class="mr-4" @click="router.go(-1)">
           <i class="fas fa-arrow-left text-primary p-3 rounded-full hover:bg-blue-50"></i>
         </button>
         <div>
-          <div class="font-extrabold text-lg">{{ currentUser.username }}</div>
-          <div class="text-sm text-gray">{{ currentUser.num_tweets }} 트윗</div>
+          <div class="font-extrabold text-lg">{{ profileUser.username }}</div>
+          <div class="text-sm text-gray">{{ profileUser.num_tweets }} 트윗</div>
         </div>
       </div>
       <!-- background image -->
       <div class="bg-gray-300 h-40 relative flex-none">
         <!-- profile image -->
         <div class="w-28 h-28 border-4 border-white bg-gray-100 rounded-full absolute -bottom-14 left-2">
-          <img :src="currentUser.profile_image_url" class="rounded-full opacity-90 hover:opacity-100 cursor-pointer" alt="">
+          <img :src="profileUser.profile_image_url" class="rounded-full opacity-90 hover:opacity-100 cursor-pointer" alt="">
         </div>
       </div>
       <!-- profile edit button -->
@@ -25,32 +25,33 @@
       </div>
       <!-- user info -->
       <div class="mx-3 mt-2">
-        <div class="font-extrabold text-lg">{{ currentUser.email }}</div>
-        <div class="text-gray">@{{ currentUser.username }}</div>
+        <div class="font-extrabold text-lg">{{ profileUser.email }}</div>
+        <div class="text-gray">@{{ profileUser.username }}</div>
         <div>
           <span class="text-gray">가입일:</span>
-          <span class="text-gray">{{ moment(currentUser.created_at).format('YYYY년 MM월') }}</span>
+          <span class="text-gray">{{ moment(profileUser.created_at).format('YYYY년 MM월') }}</span>
         </div>
         <div>
-          <span class="font-bold mr-1">{{ currentUser.followings.length }}4</span>
+          <span class="font-bold mr-1">{{ profileUser.followings.length }}4</span>
           <span class="text-gray mr-3">팔로우 중</span>
-          <span class="font-bold mr-1">{{ currentUser.followers.length }}</span>
+          <span class="font-bold mr-1">{{ profileUser.followers.length }}</span>
           <span class="text-gray">팔로워</span>
         </div>
       </div>
       <!-- tabs -->
       <div class="flex justify-between border-b border-color mt-3">
         <div @click="currentTab = 'tweet'" :class="`${currentTab == 'tweet' ? 'border-b border-primary text-primary' : 'text-gray'} py-3 text-gray w-1/3 font-bold text-center hover:bg-blue-50 cursor-pointer hover:text-primary`">트윗</div>
-        <div @click="currentTab = 'retweets'" :class="`${currentTab == 'retweets' ? 'border-b border-primary text-primary' : 'text-gray'} py-3 text-gray w-1/3 font-bold text-center hover:bg-blue-50 cursor-pointer hover:text-primary`">리트윗</div>
+        <div @click="currentTab = 'retweet'" :class="`${currentTab == 'retweet' ? 'border-b border-primary text-primary' : 'text-gray'} py-3 text-gray w-1/3 font-bold text-center hover:bg-blue-50 cursor-pointer hover:text-primary`">리트윗</div>
         <div @click="currentTab = 'like'" :class="`${currentTab == 'like' ? 'border-b border-primary text-primary' : 'text-gray'} py-3 text-gray w-1/3 font-bold text-center hover:bg-blue-50 cursor-pointer hover:text-primary`">좋아요</div>
       </div>
       <!-- tweets -->
       <div class="overflow-y-auto">
-        <Tweet v-for="tweet in currentTab == 'tweet' ? tweets : currentTab == 'retweets' ? retweets : likeTweets" :key="tweet.id" :currentUser="currentUser" :tweet="tweet"/>
+        <Tweet v-for="tweet in currentTab == 'tweet' ? tweets : currentTab == 'retweet' ? reTweets : likeTweets" :key="tweet.id" :currentUser="currentUser" :tweet="tweet"/>
       </div>
     </div>
     <!-- trend section -->
     <Trends />
+    <profile-edit-modal></profile-edit-modal>
   </div>
 </template>
 
@@ -62,17 +63,28 @@ import { computed, ref, onBeforeMount } from 'vue'
 import { TWEET_COLLECTION, USER_COLLECTION, RETWEET_COLLECTION, LIKES_COLLECTION } from '../firebase'
 import getTweetInfo from '../utils/getTweetInfo'
 import moment from 'moment'
+import { useRoute } from 'vue-router'
+import router from '../router'
+import ProfileEditModal from '../components/ProfileEditModal.vue'
 
 export default {
-  components: { Trends, Tweet },
+  components: { Trends, Tweet, ProfileEditModal },
   setup() {
     const currentUser = computed(() => store.state.user)
+    const profileUser = ref(null)
     const tweets = ref([])
-    const retweets = ref([])
+    const reTweets = ref([])
     const likeTweets = ref([])
     const currentTab = ref('tweet')
+    const route = useRoute()
 
     onBeforeMount(() => {
+      const profileUID = route.params.uid ?? currentUser.value.uid
+
+      USER_COLLECTION.doc(profileUID).onSnapshot((doc) => {
+        profileUser.value = doc.data()
+      })
+    
       USER_COLLECTION.doc(currentUser.value.uid).onSnapshot(doc => {
         store.commit("SET_USER", doc.data())
       })
@@ -92,19 +104,21 @@ export default {
 
       RETWEET_COLLECTION.where("uid", "==", currentUser.value.uid).orderBy('created_at', 'desc').onSnapshot((snapshot) => {
         snapshot.docChanges().forEach(async(change) => {
+          const doc = await TWEET_COLLECTION.doc(change.doc.data().from_tweet_id).get()
           let tweet = await getTweetInfo(change.doc.data(), currentUser.value)
           if (change.type === 'added') {
-            retweets.value.splice(change.newIndex, 0, tweet)
+            reTweets.value.splice(change.newIndex, 0, tweet)
           } else if (change.type === 'modified') {
-            retweets.value.splice(change.oldIndex, 1, tweet)
+            reTweets.value.splice(change.oldIndex, 1, tweet)
           } else if (change.type === 'removed') {
-            retweets.value.splice(change.oldIndex, 1)
+            reTweets.value.splice(change.oldIndex, 1)
           }
         })
       })
 
       LIKES_COLLECTION.where("uid", "==", currentUser.value.uid).orderBy('created_at', 'desc').onSnapshot((snapshot) => {
         snapshot.docChanges().forEach(async(change) => {
+          const doc = await TWEET_COLLECTION.doc(change.doc.data().from_tweet_id).get()
           let tweet = await getTweetInfo(change.doc.data(), currentUser.value)
           if (change.type === 'added') {
             likeTweets.value.splice(change.newIndex, 0, tweet)
@@ -121,8 +135,10 @@ export default {
       tweets,
       moment,
       currentTab,
-      retweets,
-      likeTweets
+      reTweets,
+      likeTweets,
+      profileUser,
+      router
     }
   }
 
